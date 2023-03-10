c34d21a5d3c9edad21b30d0b0d8c3d56e72908a0
Eliza Peterson <121064539+ElizaPeterson@users.noreply.github.com>
2023-03-10 13:36:06 +0000

    router dom installed

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,14 @@ import GameContainer from './Containers/GameContainer';
 import Rules from './Components/Rules';
 import Players from './Components/Players';
 import Scoreboard from './Components/Scoreboard';
+import NavBar from './Components/NavBar';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GameContainer/>
+      <NavBar/>
     </div>
   );
 }
