2fed05e2a8cd4ccfff8412852ad027ae8fd449d8
Eliza Peterson <121064539+ElizaPeterson@users.noreply.github.com>
2023-03-10 16:21:56 +0000

    new player form added

--- client/src/Components/Players.js
+++ client/src/Components/Players.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import PlayerDetails from './PlayerDetails';
 
+
 const Players = ({players, deletePlayer}) => {
+
+  const [newPlayer, setNewPlayer] = useState(  {
+    name: "",
+    active: false,
+    wins: 0,
+    losses: 0
+  })
+
+  const onChange = (event) => {
+    const formData = Object.assign({}, newPlayer);
+    formData[event.target.name] = event.target.value;
+    setNewPlayer(formData);
+    };
 
   const allPlayers = players.map((player, index) => {
     return <PlayerDetails key={index} player={player} deletePlayer={deletePlayer}/>
@@ -11,6 +25,8 @@ const Players = ({players, deletePlayer}) => {
     <div>
     <form>
       <h2>Add New Player:</h2>
+      <input type='text' name="name" value={newPlayer.name}></input>
+      <button>Add Player</button>
     </form>
         <h2>Existing Players:</h2>
         <ul>
