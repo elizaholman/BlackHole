7a044e1f3d8b8a3fa2c3a2301d09013ad00692e4
Eliza Peterson <121064539+ElizaPeterson@users.noreply.github.com>
2023-03-12 19:55:40 +0000

    homepage cssd

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -4,7 +4,7 @@ import Rules from './Rules'
 const Home = () => {
   return (
     <div className='homepage'>
-      <h1>Rules</h1>
+      <h1 className='rules'>Rules</h1>
       <h1 className='homepage-title'>Blackhole</h1>
       <Rules/>
     </div>
