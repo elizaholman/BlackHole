0f91b17df3b719a59932dd764add20cbefcbf220
Eliza Peterson <121064539+ElizaPeterson@users.noreply.github.com>
2023-03-15 14:02:23 +0000

    rules added

--- client/src/Components/Rules.js
+++ client/src/Components/Rules.js
@@ -2,10 +2,31 @@ import React from 'react'
 
 const Rules = () => {
   return (
-    <div className='rules-page'>
-      <h1><u>Game Rules!</u></h1>
-      <p>kjsdbvjksdbvkjsdbvjksdbvkjsbvkjsdb</p>
-      <p>isgciaushcahcoadhcoashcoahcoahco</p>
+    <div>
+      <div className='rules-page'>
+        <div className='rules-div'>
+          <div className='player1'>Player 1</div>
+            <h1><u>Game Rules!</u></h1>
+            <div className='player2'>Player 2</div>
+            <ul>
+              <li>
+                Orange portals are entrances (insert image)
+              </li>
+              <li>
+                Blue portals are entrances (insert image)
+              </li>
+              <li>
+                Blackhole is an entrance which can take you to any exit
+              </li>
+              <li>
+                First to 100 wins
+              </li>
+              <li>
+                Must roll exactly 100 to win
+              </li>
+            </ul>
+          </div>
+      </div>
     </div>
   )
 }
