fdd769e317e78c7195f8f03493d49a0f426e885f
Eliza Peterson <121064539+ElizaPeterson@users.noreply.github.com>
2023-03-15 15:48:25 +0000

    rules cssd

--- client/src/Components/Rules.js
+++ client/src/Components/Rules.js
@@ -2,32 +2,35 @@ import React from 'react'
 
 const Rules = () => {
   return (
-    <div>
-      <div className='rules-page'>
-        <div className='rules-div'>
-          <div className='player1'>Player 1</div>
-            <h1><u>Game Rules!</u></h1>
+        <div className='rules-page'>
+        <h1 className='title'>Black hole</h1>
+        <div className='rules-full'>
+          <div className='rules-page-top'>
+            <div className='player1'>Player 1</div>
+            <h1><u>Game Rules</u></h1>
             <div className='player2'>Player 2</div>
-            <ul>
-              <li>
-                Orange portals are entrances (insert image)
-              </li>
-              <li>
-                Blue portals are entrances (insert image)
-              </li>
-              <li>
-                Blackhole is an entrance which can take you to any exit
-              </li>
-              <li>
-                First to 100 wins
-              </li>
-              <li>
-                Must roll exactly 100 to win
-              </li>
-            </ul>
           </div>
-      </div>
-    </div>
+            <div className='rules-list-div'>
+              <ul className='rules-list'>
+                <li>
+                  Orange portals are entrances (insert image)
+                </li>
+                <li>
+                  Blue portals are exits (insert image)
+                </li>
+                <li>
+                  Black hole is an entrance which takes you to a random exit
+                </li>
+                <li>
+                  First to 100 wins
+                </li>
+                <li>
+                  Must roll exactly 100 to win
+                </li>
+              </ul>
+            </div>
+            </div>
+          </div>
   )
 }
 
